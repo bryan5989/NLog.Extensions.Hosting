bb219f73a2150a2db3c3d813f9bca5a8bfdd2a32
Bryan Gonzalez <bgonza868@gmail.com>
2018-08-01 20:02:46 -0400

    Add unit test stubs

---
+++ test/NLog.Extensions.Hosting.Tests/ExtensionMethodTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NLog.Extensions.Hosting.Tests
+{
+    [TestClass]
+    public class ExtensionMethodTests
+    {
+        [TestMethod]
+        public void UseNLog_noParams_attachesCorrectly()
+        {
+
+        }
+
+        [TestMethod]
+        public void UseNLog_noParams_returnsBuildableObject()
+        {
+
+        }
+        
+        [TestMethod]
+        public void UseNLog_withOptionsParam_attachesCorrectly()
+        {
+
+        }
+
+        [TestMethod]
+        public void UseNLog_withOptionsParam_returnsBuildableObject()
+        {
+
+        }
+    }
+}
